ab74db3e063965e8435293b2a04fe17807975f22
jackdimitri <mohdjakkur@gmail.com>
2024-06-15 12:59:58 +0530

    JakkurUpdatedcode

--- src/org/comp/Computer.java
+++ src/org/comp/Computer.java
@@ -4,5 +4,25 @@ public class Computer {
 	public void computerModel() {
 		System.out.println("Computer model is Laptop Model");
 	}
+	public void model2() {
+		System.out.println("Practice 1");
+
+	}
+	public void model4() {
+		System.out.println("Practice 1");
+	}
+	
+	public void model5() {
+		System.out.println("Practice 1");
+	}
+	
+	public void mode5() {
+		System.out.println("Practice 1");
+	}
+	
+	public void model6() {
+		System.out.println("Practice 1");
+	}
+	
 
 }
